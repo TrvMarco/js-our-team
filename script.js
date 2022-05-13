61d6404e3b3ff4b63e015dd5913ded52d15882f5
TrvMarco <trevisanello.marco@gmail.com>
2022-05-13 15:52:34 +0200

    aggiunto tutti i membri del team

--- script.js
+++ script.js
@@ -2,11 +2,35 @@
 const teamMembers = [
 
     {
-        memberPicture: "",
+        memberPicture: "img/wayne-barnett-founder-ceo.jpg",
         memberName: "Wayne Barnett",
         memberRole: "Founder & CEO"
     }, 
-
+    {
+        memberPicture: "img/angela-caroll-chief-editor.jpg",
+        memberName: "Angela Caroll",
+        memberRole: "Chief Editor"
+    },
+    {
+        memberPicture: "img/walter-gordon-office-manager.jpg",
+        memberName: "Walter Gordon",
+        memberRole: "Office Manager"
+    },
+    {
+        memberPicture: "img/angela-lopez-social-media-manager.jpg",
+        memberName: "Angela Lopez",
+        memberRole: "Social Media Manager"
+    },
+    {
+        memberPicture: "img/scott-estrada-developer.jpg",
+        memberName: "Scott Estrada",
+        memberRole: "Developer"
+    },
+    {
+        memberPicture: "img/barbara-ramos-graphic-designer.jpg",
+        memberName: "Barbara Ramos",
+        memberRole: "Graphic Designer"
+    },
 ];
 
 const teamContainer = document.querySelector(".team-container");
@@ -16,14 +40,13 @@ for(let i = 0; i < teamMembers.length; i ++){
     const teamCard = ` 
     <div class="team-card">
         <div class="card-image">
-        <img src="img/wayne-barnett-founder-ceo.jpg"
-            alt="Wayne Barnett"
-        />
+            <img src="${teamMembers[i].memberPicture}"
+                alt="${teamMembers[i].memberName}"
+            />
         </div>
         <div class="card-text">
-        <h3>Wayne Barnett</h3>
-        <p>Founder & CEO</p>
-        </div>
+        <h3>${teamMembers[i].memberName}</h3>
+        <p>${teamMembers[i].memberRole}</p>   
     </div>`
 
     teamContainer.innerHTML += teamCard
